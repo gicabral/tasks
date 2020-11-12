60fa5c0ea0bcfa150c03d498fcaaeabf735bd07a
Giovanna Cabral <38368320+gicabral@users.noreply.github.com>
2020-11-12 00:56:14 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 python3 -m pip install -r tasks/requirements.txt
-python3 tasks/manage.py migrate
+python3 manage.py migrate
 echo '@reboot cd /home/ubuntu/tasks && ./run.sh' | crontab
 export DJANGO_SUPERUSER_PASSWORD=cloud
 export DJANGO_SUPERUSER_USERNAME=cloud
 export DJANGO_SUPERUSER_EMAIL=cloud@a.com
-python3 tasks/manage.py createsuperuser --noinput
+python3 manage.py createsuperuser --noinput
