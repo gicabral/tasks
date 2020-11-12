c54a86c6128bb0af30baa86ca3c4225c6d47334b
Giovanna Cabral <38368320+gicabral@users.noreply.github.com>
2020-11-12 00:58:37 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python3 -m pip install -r tasks/requirements.txt
+python3 -m pip install -r requirements.txt
 python3 manage.py migrate
 echo '@reboot cd /home/ubuntu/tasks && ./run.sh' | crontab
 export DJANGO_SUPERUSER_PASSWORD=cloud
