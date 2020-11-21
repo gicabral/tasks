4768b8b41603e277e6241cc5756023f630cd1187
Giovanna Cabral <38368320+gicabral@users.noreply.github.com>
2020-11-20 21:52:01 -0300

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -9,26 +9,20 @@ from tasks.serializer import TaskSerializer
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
 
 class TaskCreate(generics.CreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    return HttpResponse("Task created succesfully")
 
 class TaskList(generics.ListAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
      _json = serializers.serialize("json", queryset)
-    return HttpResponse(_json, content_type="application/json")
 
 class TaskUpdate(generics.RetrieveUpdateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    return HttpResponse("Task updated succesfully")
 
 class TaskDelete(generics.DestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer 
-    return HttpResponse("Task deleted succesfully")
