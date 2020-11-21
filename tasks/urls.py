cb1a6ba4e9e4bc0280037152809ad7facbbd8cde
Giovanna Cabral <38368320+gicabral@users.noreply.github.com>
2020-11-21 16:20:52 -0300

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from tasks import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path("listar", views.getList, name="AllTasks"),
     path('atualizar/<int:pk>', views.updateTask, name="ChangeTask"),
     path('adicionar', views.createTask, name="createTask"),
-    path('deletar', views.delete_all, name="deleteTask"),
+    path('deletar/<int:pk>', views.delete_all, name="deleteTask"),
 ]
 
