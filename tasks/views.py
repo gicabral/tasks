2629551aa383c3c775875769100c28dd8cda7e98
Giovanna Cabral <38368320+gicabral@users.noreply.github.com>
2020-11-21 03:20:41 -0300

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -15,8 +15,18 @@ def index(request):
 class TaskCreate(generics.CreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    def create(request):
-        return HttpResponse(json.dumps({'Status': 'Success', 'Message': 'Task successfully created.', 'Id': queryset.pk}))
+     def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.DATA, files=request.FILES)
+
+        if serializer.is_valid():
+            self.pre_save(serializer.object)
+            self.object = serializer.save(force_insert=True)
+            self.post_save(self.object, created=True)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED,
+                            headers=headers)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 class TaskList(generics.ListAPIView):
     queryset = Task.objects.all()
