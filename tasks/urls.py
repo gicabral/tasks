999c20c0dcafe649e703d85f777cc3bb3fd5b634
Giovanna Cabral <38368320+gicabral@users.noreply.github.com>
2020-11-21 17:08:35 -0300

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,9 +4,9 @@ from tasks import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("listar", views.getList, name="AllTasks"),
-    path('atualizar/<int:pk>', views.updateTask, name="ChangeTask"),
-    path('adicionar', views.createTask, name="createTask"),
-    path('deletar/<int:pk>', views.delete_all, name="deleteTask"),
+    path("listar", views.listar, name="listarTasks"),
+    path('atualizar/<int:pk>', views.atualizar, name="atualizarTask"),
+    path('adicionar', views.adicionar, name="adicionarTask"),
+    path('deletar/<int:pk>', views.deletar, name="deletarTask"),
 ]
 
